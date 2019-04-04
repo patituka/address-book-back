8029aba73cb5fae4959f6056f19ab3db298bcea6
Patricia Leclerc <patricia.leclerc@live.fr>
2019-04-04 16:27:40 +0200

    toto

--- addressbook/src/main/java/fr/formation/addressbook/controllers/AddressController.java
+++ addressbook/src/main/java/fr/formation/addressbook/controllers/AddressController.java
@@ -1,33 +1,27 @@
 package fr.formation.addressbook.controllers;
 
 import javax.validation.Valid;
-import javax.xml.ws.Service;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.formation.addressbook.dtos.AddressPartCreateDto;
-import fr.formation.addressbook.services.AddressPartService;
+import fr.formation.addressbook.dtos.AddressDto;
+import fr.formation.addressbook.services.AddressService;
 
 @RestController
 @RequestMapping("/addresses")
 public class AddressController {
-	
-	private final AddressPartService service;
-	
-	public AddressController (AddressPartService service) {
-		this.service = service;
-	}
-	
 
-	@PostMapping ("/particular")
-	protected void create (@Valid @RequestBody AddressPartCreateDto addressPart) {
-		service.create(addressPart);
-	}
-	
-	
-	
-	
+    private final AddressService service;
+
+    public AddressController(AddressService service) {
+	this.service = service;
+    }
+
+    @PostMapping("/particular")
+    protected void create(@Valid @RequestBody AddressDto addressPart) {
+	service.create(addressPart);
+    }
 }
