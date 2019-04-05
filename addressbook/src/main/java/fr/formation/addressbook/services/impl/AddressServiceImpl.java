51edb754ec35ab9c7f7d2555d4c905524efcb50c
Remy <guillouxrem@gmail.com>
2019-04-05 15:24:03 +0200

    logger error buffereader

--- addressbook/src/main/java/fr/formation/addressbook/services/impl/AddressServiceImpl.java
+++ addressbook/src/main/java/fr/formation/addressbook/services/impl/AddressServiceImpl.java
@@ -2,6 +2,8 @@ package fr.formation.addressbook.services.impl;
 
 import javax.validation.Valid;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,10 +12,11 @@ import fr.formation.addressbook.dtos.AddressDto;
 import fr.formation.addressbook.entities.Address;
 import fr.formation.addressbook.repositories.AddressJpaRepository;
 import fr.formation.addressbook.services.AddressService;
+import fr.formation.addressbook.utils.CsvReader;
 
 @Service
 public class AddressServiceImpl implements AddressService {
-
+	private static final Logger logger = LogManager.getLogger(AddressServiceImpl.class);
     @Autowired
     private AddressJpaRepository repo;
 
@@ -24,5 +27,6 @@ public class AddressServiceImpl implements AddressService {
     public void create(@Valid AddressDto dto) {
 	Address address = mapper.map(dto, Address.class);
 	repo.save(address);
+	
     }
 }
